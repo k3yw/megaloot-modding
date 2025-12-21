d7e8d08a919bf24f86240ee410bc827da12ee8c1
Anon <anon@me.com>
2025-12-21 08:57:03 +0700

    New BBCode description for armored attack

--- resources/bb_scripts/adaptive_attack.gd
+++ resources/bb_scripts/adaptive_attack.gd
@@ -7,10 +7,13 @@ extends BBScript
 
 
 
-
 func get_bb_container_data(_args: Array = []) -> Array[BBContainerData]:
-    var bb_container_data: Array[BBContainerData] = []
+	var bb_container_data: Array[BBContainerData] = []
 
-    bb_container_data.push_back(BBContainerData.new("Increases your current attack damage"))
+	bb_container_data.push_back(BBContainerData.new("Converts % of your ", Color.DARK_GRAY))
+	bb_container_data.push_back(Stats.get_bb_container_data(Stats.ACTIVE_ARMOR))
+	bb_container_data.push_back(BBContainerData.new(" to ", Color.DARK_GRAY))
+	bb_container_data.push_back(Stats.get_bb_container_data(Stats.ARMOR_DAMAGE))
+	bb_container_data.push_back(BBContainerData.new(" dealt on hit", Color.DARK_GRAY))
 
-    return bb_container_data
+	return bb_container_data
